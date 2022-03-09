d046523bedc5ff0bdd78a061adce69a95bfae2aa
João Inácio <egnawake@gmail.com>
2022-03-09 22:15:18 +0000

    PercorreString: get user input

---
+++ PercorreString/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace PercorreString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inp, text;
+            char excluded;
+
+            // get string from user
+            Console.WriteLine("Enter a string:");
+            inp = Console.ReadLine();
+            text = inp;
+
+            // get char from user
+            Console.WriteLine("Enter a single character:");
+            inp = Console.ReadLine();
+            excluded = char.Parse(inp);
+
+            Console.WriteLine($"User entered: {text}; {excluded}");
+        }
+    }
+}
